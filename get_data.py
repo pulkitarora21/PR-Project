5763e96b741e5b67d012b11bfd5bc2734dcf934f
Pulkit <pulkit12082@iiitd.ac.in>
2015-03-02 04:21:19 +0530

    gets top 100 stories. but how should we get data for our project?

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 import json, pycurl, StringIO
-url = "https://hacker-news.firebaseio.com/v0/user/jl.json"
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 c = pycurl.Curl()
 c.setopt(pycurl.URL, url)
 c.setopt(pycurl.SSL_VERIFYPEER, 0)
@@ -14,3 +14,20 @@ htmlsrc = b.getvalue()
 json_data=htmlsrc
 data = json.loads(json_data)
 print data
+for i in data:
+	url = "https://hacker-news.firebaseio.com/v0/item/"+str(i)+".json"
+	c = pycurl.Curl()
+	c.setopt(pycurl.URL, url)
+	c.setopt(pycurl.SSL_VERIFYPEER, 0)
+	c.setopt(pycurl.SSL_VERIFYHOST, 0)
+	b = StringIO.StringIO()
+	c.setopt(pycurl.WRITEFUNCTION, b.write)
+	c.setopt(pycurl.FOLLOWLOCATION, 1)
+	c.setopt(pycurl.MAXREDIRS, 5)
+	c.perform()
+	htmlsrc = b.getvalue()
+
+	json_data=htmlsrc
+	data1 = json.loads(json_data)
+	print data1
+	
