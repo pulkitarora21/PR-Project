51a50337e256e8476d4d545c222d7a9b3b9e2a27
Pulkit <pulkit12082@iiitd.ac.in>
2015-03-25 03:55:29 +0530

    time_slot added in feature extraction

--- feature_extraction2.py
+++ feature_extraction2.py
@@ -14,7 +14,7 @@ def histogram(words, freq):
 
 
 
-json_data=open('stories1.txt')
+json_data=open('stories2_karma.txt')
 data = json.load(json_data)
 
 
@@ -22,7 +22,7 @@ data = json.load(json_data)
 for i in range(0,len(data)):
     data[i]["datetime"]=data[i]["time"] # store the existing datetime (which is stored in time in the existing stories) in date 
     data[i]["time"]=data[i]["time"]%86400 # extract only the time component from unix datetime
-    
+    data[i]["time_slot"]=data[i]["time"]/1800
     try:
         data[i]["netloc"]=urlparse(data[i]["url"]).netloc # extract server name from the url - www.website.com from http://www.website.com/directory/..
     except KeyError:
@@ -57,6 +57,6 @@ for i in range(0,len(data)):
     data[i]["words_freq"] = freq
 
 
-with open('stories1_feature_extraction.txt', 'w') as outfile:
+with open('stories2_feature_extraction.txt', 'w') as outfile:
         json.dump(data, outfile, indent=4)
 
